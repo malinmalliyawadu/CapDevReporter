4248bd973badc9adeeff9af3536e8eb36566f87f
Malin Malliya Wadu <malin.malliya.wadu@ptml.com>
2025-02-20 13:50:17 +1300

    Copy prisma stuff in nextjs build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,25 @@
 import MillionLint from "@million/lint";
 import type { NextConfig } from "next";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
+  webpack: (config, { isServer }) => {
+    // add copy webpack plugin
+    if (isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              // copy the templates folder
+              from: "prisma/",
+              to: "prisma/",
+            },
+          ],
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default process.env.DISABLE_MILLION
