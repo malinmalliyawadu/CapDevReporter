6981f9fbb0ec26d272b7af3ed44e6da68f518300
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2025-02-20 13:14:47 +1300

    Show error

--- src/app/api/general-time-types/route.ts
+++ src/app/api/general-time-types/route.ts
@@ -71,9 +71,6 @@ export async function POST(request: Request) {
     return NextResponse.json(timeType);
   } catch (error) {
     console.error(error);
-    return NextResponse.json(
-      { error: "Failed to create time type" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
